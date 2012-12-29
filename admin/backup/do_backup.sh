a200f388c19333925dd592b10c77a56a48a1eb4b
Todd Harris <info@toddharris.net>
2012-12-29 14:17:17 -0800

    * added backup directory; organized admin

---
+++ admin/backup/do_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###########################################
+# Backup WormBase information
+###########################################
+
+BACKUP_HOST=wb-dev.oicr.on.ca
+BACKUP_USER=tharris
+
+# Which host are we running on?
+THIS_HOST=`hostname`
+DATE=`date +%Y-%m-%d`
+
+#######################
+#  WORMBOOK / THE WBG
+#######################
+# Create a suitable backup directory
+echo "Backing up WormBook to ${BACKUP_HOST}";
+
+# Dump the mysql database for the wbg
+/usr/bin/mysqldump  \
+    -h mysql.wormbook.org \
+    -u wormbook -p3l3g@nz  wordpress_wormbook_gazette | \
+    gzip -c > /home/tharris70/mysqldump/${DATE}-wormbook_wordpress-worm_breeders_gazette.sql.gz
+
+
+    # Rsync the site directory for easy restoration
+    # No reason to maintain daily backups; 1 copy is sufficient.
+#     rsync -avv --rsh=ssh --exclude logs/ /usr/local/bookworm/ \
+#	  ${BACKUP_USER}@${BACKUP_HOST}:backups/wormbook/production/.
+rsync -avv --rsh=ssh --exclude Maildir/ --exclude logs/ /home/tharris70/ \
+     ${BACKUP_USER}@${BACKUP_HOST}:backups/wormbook/dreamhost/.
+
